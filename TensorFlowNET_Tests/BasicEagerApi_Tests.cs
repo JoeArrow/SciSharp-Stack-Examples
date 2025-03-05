3c18ba187e2621ba857609fe47bb3298f30017e2
JoeArrow <joearrow1964@gmail.com>
2025-03-05 08:09:23 -0500

    Converting to class library

--- TensorFlowNET_Tests/BasicEagerApi_Tests.cs
+++ TensorFlowNET_Tests/BasicEagerApi_Tests.cs
@@ -1,4 +1,4 @@
-﻿#region © 2025 Aflac.
+﻿#region © 2025 Joe Arrowood (JoeWare)
 //
 // All rights reserved. Reproduction or transmission in whole or in part, in
 // any form or by any means, electronic, mechanical, or otherwise, is prohibited
